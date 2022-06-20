3c6953fb8cac7eda4a83416e83d6c65717bc87fa
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-20 11:52:39 +0800

    build(examples): use symlink to the library

--- FabricExample/metro.config.js
+++ FabricExample/metro.config.js
@@ -1,11 +1,11 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
+
+// Use custom root so Metro can resolve a symlink to our library
+const root = path.resolve(__dirname, '..');
 
 module.exports = {
+  projectRoot: __dirname,
+  watchFolders: [root],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
--- PaperExample/metro.config.js
+++ PaperExample/metro.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
+// Use custom root so Metro can resolve a symlink to our library
+const root = path.resolve(__dirname, '..');
+
 module.exports = {
-  getProjectRoots() {
-    return [path.resolve(__dirname), path.resolve(__dirname, '../')];
-  },
+  projectRoot: __dirname,
+  watchFolders: [root],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
