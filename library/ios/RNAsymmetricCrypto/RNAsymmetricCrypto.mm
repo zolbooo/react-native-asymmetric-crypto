54a53ec1878e34685b43fe0581f87f2e8082a6cb
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-20 10:43:01 +0800

    feat(ios): add new arch code

--- library/ios/RNAsymmetricCrypto/RNAsymmetricCrypto.mm
+++ library/ios/RNAsymmetricCrypto/RNAsymmetricCrypto.mm
@@ -1,7 +1,17 @@
 #import "RNAsymmetricCrypto.h"
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNCalculatorSpec.h"
+#endif
 
 @implementation RNAsymmetricCrypto
 
 RCT_EXPORT_MODULE(AsymmetricCrypto)
 
+#ifdef RCT_NEW_ARCH_ENABLED
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+    return std::make_shared<facebook::react::NativeCalculatorSpecJSI>(params);
+}
+#endif
 @end
