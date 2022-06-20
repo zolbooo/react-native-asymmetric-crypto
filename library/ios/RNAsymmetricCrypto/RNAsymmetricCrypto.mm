5b6784765111224abb4958e438d48487031ef9a2
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-20 11:20:49 +0800

    fix(ios): RCT_EXPORT_MODULE statement

--- library/ios/RNAsymmetricCrypto/RNAsymmetricCrypto.mm
+++ library/ios/RNAsymmetricCrypto/RNAsymmetricCrypto.mm
@@ -5,7 +5,7 @@
 
 @implementation RNAsymmetricCrypto
 
-RCT_EXPORT_MODULE(AsymmetricCrypto)
+RCT_EXPORT_MODULE(RNAsymmetricCrypto)
 
 #ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
