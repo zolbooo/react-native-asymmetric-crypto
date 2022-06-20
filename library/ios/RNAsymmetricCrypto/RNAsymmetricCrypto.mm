2185028cb00db1006f69d6f86ff0541e40e399c7
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-20 11:18:52 +0800

    fix(ios): JSI module usage

--- library/ios/RNAsymmetricCrypto/RNAsymmetricCrypto.mm
+++ library/ios/RNAsymmetricCrypto/RNAsymmetricCrypto.mm
@@ -11,7 +11,7 @@ RCT_EXPORT_MODULE(AsymmetricCrypto)
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
-    return std::make_shared<facebook::react::NativeCalculatorSpecJSI>(params);
+    return std::make_shared<facebook::react::NativeRNAsymmetricCryptoSpecJSI>(params);
 }
 #endif
 @end
