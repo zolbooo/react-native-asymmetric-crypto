8968abc20980d9bcefa137ffaacbdb37a4f21711
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-21 23:16:22 +0800

    perf(get-available-biometry-type): use dispatch_async

--- library/ios/RNAsymmetricCrypto/RNAsymmetricCrypto.mm
+++ library/ios/RNAsymmetricCrypto/RNAsymmetricCrypto.mm
@@ -8,37 +8,38 @@ RCT_EXPORT_MODULE(RNAsymmetricCrypto)
 
 RCT_EXPORT_METHOD(getAvailableBiometryType: (RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject) {
-    LAContext *context = [[LAContext alloc] init];
-    NSError *la_error = nil;
-    LAPolicy laPolicy = LAPolicyDeviceOwnerAuthenticationWithBiometrics;
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        LAContext *context = [[LAContext alloc] init];
+        NSError *la_error = nil;
+        LAPolicy laPolicy = LAPolicyDeviceOwnerAuthenticationWithBiometrics;
+        BOOL canEvaluatePolicy = [context canEvaluatePolicy:laPolicy error:&la_error];
+        if (canEvaluatePolicy) {
+            NSString *biometryType;
+            if (@available(iOS 11, *)) {
+                biometryType = (context.biometryType == LABiometryTypeFaceID) ? @"FaceID" : @"TouchID";
+            } else {
+                biometryType = @"TouchID";
+            }
 
-    BOOL canEvaluatePolicy = [context canEvaluatePolicy:laPolicy error:&la_error];
-    if (canEvaluatePolicy) {
-        NSString *biometryType;
-        if (@available(iOS 11, *)) {
-            biometryType = (context.biometryType == LABiometryTypeFaceID) ? @"FaceID" : @"TouchID";
+            NSDictionary *result = @{
+                @"available": @(YES),
+                @"biometryType": biometryType
+            };
+            resolve(result);
         } else {
-            biometryType = @"TouchID";
+            /*
+            TODO: Some error messages could be useful, for example:
+            - No fingerprint/face identities enrolled
+            In this case we should be able to provide some enum value to the JS side
+            */
+            NSString *errorMessage = [NSString stringWithFormat:@"%@", la_error];
+            NSDictionary *result = @{
+                @"available": @(NO),
+                @"error": errorMessage
+            };
+            resolve(result);
         }
-
-        NSDictionary *result = @{
-            @"available": @(YES),
-            @"biometryType": biometryType
-        };
-        resolve(result);
-    } else {
-        /*
-        TODO: Some error messages could be useful, for example:
-        - No fingerprint/face identities enrolled
-        In this case we should be able to provide some enum value to the JS side
-        */
-        NSString *errorMessage = [NSString stringWithFormat:@"%@", la_error];
-        NSDictionary *result = @{
-            @"available": @(NO),
-            @"error": errorMessage
-        };
-        resolve(result);
-    }
+    });
 }
 
 #ifdef RCT_NEW_ARCH_ENABLED
