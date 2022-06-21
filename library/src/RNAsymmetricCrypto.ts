2abedc4f0ea66aa89337cfc223044111215aefb4
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-21 22:14:13 +0800

    fix(paper): import native module properly

--- library/src/RNAsymmetricCrypto.ts
+++ library/src/RNAsymmetricCrypto.ts
@@ -2,7 +2,6 @@ import { NativeModules } from "react-native";
 
 import type { Spec } from "./NativeRNAsymmetricCrypto";
 
-const RNAsymmetricCryptoModule = NativeModules.RNAsymmetricCrypto;
+const RNAsymmetricCrypto: Spec = NativeModules.RNAsymmetricCrypto;
 
-const RNAsymmetricCrypto: Spec = new RNAsymmetricCryptoModule();
 export default RNAsymmetricCrypto;
